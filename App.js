53ed034215ac52f9d3fcd299304c67e60234ace2
Juan Pablo <juan.eslava@ergosign.de>
2020-12-26 19:18:30 +0100

    hola ejposa

--- App.js
+++ App.js
@@ -1,8 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+
+  useEffect( () => {
+    console.log('hi ejposa')
+  }, [])
+
   return (
     <View style={styles.container}>
       <Text>Hola Ejposa</Text>
