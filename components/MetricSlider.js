61a153468145bdbc67b3560a2d138e7bd5f854fd
Juan Pablo <juan.eslava@ergosign.de>
2020-12-27 22:26:11 +0100

    comp: updated MetricSlider (installed Slider from @react-native-community

--- components/MetricSlider.js
+++ components/MetricSlider.js
@@ -1,11 +1,26 @@
+// npm install @react-native-community/slider --save
 import React from 'react';
 // react native comps
-import { View, Text } from "react-native";
+import { View, Text } from 'react-native'
+import Slider from '@react-native-community/slider';
 
-const MetricSlider = () => {
+const MetricSlider = (props) => {
+
+    const { max, unit, step, value, onChange } = props
+    
     return (
         <View>
-            <Text> MetricSlider </Text>
+            <Slider 
+                step={step}
+                value={value}
+                maximumValue={max}
+                minimumValue={0} 
+                onValueChange={onChange}
+            />
+            <View>
+                <Text>{value}</Text>
+                <Text>{unit}</Text>
+            </View>
         </View>
     )
 }
