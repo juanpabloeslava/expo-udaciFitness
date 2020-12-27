5546a596a82a05ac5c69baf0ae76e92b9ee6f731
Juan Pablo <juan.eslava@ergosign.de>
2020-12-27 16:13:14 +0100

    comp: addEntry: renders sliders or steppers depending on each entry's type

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -19,7 +19,7 @@ const AddEntry = () => {
         eat: 0,
     });
 
-    useEffect( () => {
+    useEffect(() => {
         console.log('state: ', state)
     }, [])
 
@@ -32,12 +32,12 @@ const AddEntry = () => {
             //do not modifiy the original state, instead, return a copy
             ...state,
             // return the new value for the metric: if it's greater than the max value, return the max value, if it's not, then increase
-            [metric]: increase > max 
+            [metric]: increase > max
                 ? max
                 : increase
         })
     }
-    
+
     const decrement = (metric) => {
         const { step } = getMetricMetaInfo(metric)
         //  decrease the metric with the step value
@@ -51,19 +51,51 @@ const AddEntry = () => {
         })
     }
 
-    const slider = ( metric, value) => {
+    const slider = (metric, value) => {
         setState({
             [metric]: value
         })
     }
 
-    return ( 
+    const metaInfo = getMetricMetaInfo()
+
+    return (
         <View>
-            <Text> Add Entry Component </Text>
-            <MetricSlider />
-            <MetricStepper />
+            {
+                // make an array of the properties of the metaInfo object. 
+                Object.keys(metaInfo).map(key => {
+                    const { getIcon, type, ...rest } = metaInfo[key]
+                    const value = state[key]
+                    // For each element of the array, return that item's icon, and render either a stepper of a slider depending on its type 
+                    return (
+                        <View key={key}>
+                            {
+                                // element's icon
+                                getIcon()
+                            }
+                            {
+                                // slider or stepper
+                                type === 'slider'
+                                    ? <MetricSlider
+                                        value={value}
+                                        onChange={value => slider(key, value)}
+                                        {...rest}
+                                    />
+                                    : <MetricStepper
+                                        value={value}
+                                        onIncrement={ () => increment(key)}
+                                        onDecrement={ () => decrement(key)}
+                                        {...rest}
+                                    />
+                            }
+
+                        </View>
+                    )
+
+                })
+            }
         </View>
-     )
+    )
 }
- 
+
 export default AddEntry
