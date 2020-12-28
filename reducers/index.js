1296c378c4ce9d2889ee3705e24c4b45066d283e
Juan Pablo <juan.eslava@ergosign.de>
2020-12-28 15:30:26 +0100

    reducers: added entries Reducer

---
+++ reducers/index.js
@@ -0,0 +1,21 @@
+import { RECEIVE_ENTRIES, ADD_ENTRY } from '../actions/actionTypes'
+
+const entriesReducer = (state = {}, action) => {
+  switch (action.type) {
+    case RECEIVE_ENTRIES :
+      return {
+        ...state,
+        // spread all of the action keys into the new state object. We only have one key, but it's a safeguard in case more keys are added to it, they won't cause any trouble
+        ...action.entries,
+      }
+    case ADD_ENTRY :
+      return {
+        ...state,
+        ...action.entry
+      }
+    default :
+      return state
+  }
+}
+
+export default entriesReducer
