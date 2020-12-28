b46e47ab5dd0967f3aafb6ca7fb76f2a57d600b9
Juan Pablo <juan.eslava@ergosign.de>
2020-12-28 15:37:36 +0100

    store: added redux store

--- App.js
+++ App.js
@@ -1,18 +1,23 @@
 // yarn add @react-native-async-storage/async-storage
+// yarn add redux react-redux
 
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { View, Text } from 'react-native';
 // comps
 import AddEntry from './components/AddEntry'
-
+// redux
+import { Provider } from 'react-redux'
+import store from './store'
 
 export default function App() {
 
   return (
-    <View>
-      <AddEntry />
-    </View>
+    <Provider store = {store} >
+      <View>
+        <AddEntry />
+      </View>
+    </Provider>
   )
 
 }
